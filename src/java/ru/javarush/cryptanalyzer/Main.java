dc98bc2c3443d69db75a33b3ed4d83710d6eae29
OwUser <segich94@gmail.com>
2022-03-14 18:25:10 +0300

    Upgrade and test Crypt procedure

--- src/java/ru/javarush/cryptanalyzer/Main.java
+++ src/java/ru/javarush/cryptanalyzer/Main.java
@@ -1,9 +1,17 @@
 package ru.javarush.cryptanalyzer;
 
+import ru.javarush.cryptanalyzer.cryptography.Crypt;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        String ish = "Мама мыла раму!";
+        Crypt crypt = new Crypt(ish,3);
+        crypt.encrypt();
 
+        String ish2 = "Пгпг'пюог'угпц,";
+        Crypt crypt1 = new Crypt(ish2,3);
+        crypt1.decrypt();
     }
 }
--- src/java/ru/javarush/cryptanalyzer/cryptography/Crypt.java
+++ src/java/ru/javarush/cryptanalyzer/cryptography/Crypt.java
@@ -3,26 +3,35 @@ package ru.javarush.cryptanalyzer.cryptography;
 import java.util.Arrays;
 
 public class Crypt {
-    private static int shift = 0;
+    private String data;
+    private int cryptShift;
     private static final char[] ALPHABET = {' ', '!', '"', '\'', ',', '.', ':', '?', '«', '»',
             'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р',
             'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б',
             'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
             'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
 
-    public static void setShift(int shift) {
-        Crypt.shift = shift;
+
+    public Crypt(String data, int cryptShift) {
+        this.data = data;
+        this.cryptShift = cryptShift;
     }
 
     public void encrypt(){
-
+        for (char ch:this.data.toCharArray()) {
+            System.out.print(cryptChar(ch,cryptShift));
+        }
     }
-    public void decrypt(){}
+    public void decrypt(){
+        for (char ch:this.data.toCharArray()) {
+            System.out.print(cryptChar(ch,cryptShift * (-1)));
+        }
+    }
 
-    public char cryptChar(char ch){
+    public char cryptChar(char ch, int shift){
         int searchResult = Arrays.binarySearch(ALPHABET,ch);
         if (searchResult == -1)
             return ch;
-        else return (char) ((searchResult+shift) % ALPHABET.length);
+        else return  ALPHABET[(searchResult+shift) % ALPHABET.length];
     }
 }
